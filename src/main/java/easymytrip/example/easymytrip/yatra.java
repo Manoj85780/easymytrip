3af17a3540b6529e3bd412cf67844c6c3698c5f4
Manoj85780 <manojjadhav083@gmail.com>
2024-05-20 20:46:28 +0530

    CodePush

---
+++ src/main/java/easymytrip/example/easymytrip/yatra.java
@@ -0,0 +1,11 @@
+package easymytrip.example.easymytrip;
+
+import org.springframework.boot.SpringApplication;
+
+public class yatra {
+
+    public static void main(String[] args) { SpringApplication.run(yatra.class, args); }
+
+}
+
+
